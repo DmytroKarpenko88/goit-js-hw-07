4707d40740ce64990b3ece26796fad4c6a190b21
Dmytro Karpenko <kardmitriy@gmail.com>
2023-02-08 19:41:29 +0200

    task 1-2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryMarkup = createGalleryCards(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+galleryContainer.addEventListener('click', onImageClick);
+
+function createGalleryCards(items) {
+  return items
+    .map(
+      ({ description, original, preview }) => `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        
+      />
+    </a>
+  </div>  `
+    )
+    .join('');
+}
+
+function onImageClick(event) {
+  event.preventDefault();
+  const link = event.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+      <img src="${link}" width="800" height="600">
+  `);
+
+  instance.show();
+
+  galleryContainer.addEventListener('keydown', (event) => {
+    if (event.code === 'Escape') {
+      instance.close();
+    }
+  });
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryMarkup = createGalleryCards(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+galleryContainer.addEventListener('click', onImageClick);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  animationSlide: false,
+  captionsData: 'alt',
+  captionDelay: 500,
+});
+
+function createGalleryCards(items) {
+  return items
+    .map(
+      ({
+        description,
+        original,
+        preview,
+      }) => `<a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a> `
+    )
+    .join('');
+}
+
+function onImageClick(event) {
+  event.preventDefault();
+}
